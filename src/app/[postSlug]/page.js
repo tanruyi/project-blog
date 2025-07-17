ee23b242d91b14f019edda026ac77f7cb7fe4f52
ruyi <tan_ruyi@hotmail.com>
2025-07-17 13:52:41 +0800

    - exercise 3 done

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,11 +3,20 @@ import React from 'react';
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
-import { loadBlogPost } from '@/helpers/file-helpers';
+import { getBlogPost } from '@/helpers/file-helpers';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
+export async function generateMetadata({ params }) {
+	var { frontmatter } = await getBlogPost(params.postSlug);
+
+	return {
+		title: frontmatter.title,
+		description: frontmatter.abstract,
+	};
+}
+
 async function BlogPost({ params }) {
-	var { frontmatter, content } = await loadBlogPost(params.postSlug);
+	var { frontmatter, content } = await getBlogPost(params.postSlug);
 
 	return (
 		<article className={styles.wrapper}>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,47 +1,41 @@
 import React from 'react';
-import {
-  Work_Sans,
-  Spline_Sans_Mono,
-} from 'next/font/google';
+import { Work_Sans, Spline_Sans_Mono } from 'next/font/google';
 import clsx from 'clsx';
 
-import { LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
+import { LIGHT_TOKENS, DARK_TOKENS, BLOG_TITLE, BLOG_DESCRIPTION } from '@/constants';
 
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './styles.css';
 
 const mainFont = Work_Sans({
-  subsets: ['latin'],
-  display: 'fallback',
-  weight: 'variable',
-  variable: '--font-family',
+	subsets: ['latin'],
+	display: 'fallback',
+	weight: 'variable',
+	variable: '--font-family',
 });
 const monoFont = Spline_Sans_Mono({
-  subsets: ['latin'],
-  display: 'fallback',
-  weight: 'variable',
-  variable: '--font-family-mono',
+	subsets: ['latin'],
+	display: 'fallback',
+	weight: 'variable',
+	variable: '--font-family-mono',
 });
 
-function RootLayout({ children }) {
-  // TODO: Dynamic theme depending on user preference
-  const theme = 'light';
+export const metadata = { title: BLOG_TITLE, description: BLOG_DESCRIPTION };
 
-  return (
-    <html
-      lang="en"
-      className={clsx(mainFont.variable, monoFont.variable)}
-      data-color-theme={theme}
-      style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
-    >
-      <body>
-        <Header theme={theme} />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
-  );
+function RootLayout({ children }) {
+	// TODO: Dynamic theme depending on user preference
+	const theme = 'light';
+
+	return (
+		<html lang='en' className={clsx(mainFont.variable, monoFont.variable)} data-color-theme={theme} style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}>
+			<body>
+				<Header theme={theme} />
+				<main>{children}</main>
+				<Footer />
+			</body>
+		</html>
+	);
 }
 
 export default RootLayout;
